7f4a7ae9be862d4de8415d2887b2b81a7f3cbded
Brad Sileo <brad@sileo.name>
2021-03-24 10:05:26 -0400

    System startup for API

--- Fireplace/fireplace_api.sh
+++ Fireplace/fireplace_api.sh
@@ -1,4 +1,7 @@
-export FLASK_APP=fireplace_qt.py
-flask run --host=0.0.0.0 >> ~/flask_api.log
+#!/bin/bash
+
+export FLASK_APP=~/HomeAutomation/Fireplace/fireplace_qt.py
+cd /home/pi/HomeAutomation/Fireplace
+/usr/bin/flask run --host=0.0.0.0 >> ~/flask_api.log
 
 
